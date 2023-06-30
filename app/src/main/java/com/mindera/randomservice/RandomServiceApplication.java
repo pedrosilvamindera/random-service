bfa37f8874f979b37e9b7e68b29542e1511c77a5
Pedro Silva <pedro.silva2@kingfisher.com>
2023-06-30 17:04:09 +0100

    initial commit

---
+++ app/src/main/java/com/mindera/randomservice/RandomServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mindera.randomservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandomServiceApplication.class, args);
+	}
+
+}

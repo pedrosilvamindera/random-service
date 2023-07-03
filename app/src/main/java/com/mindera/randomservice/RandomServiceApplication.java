9cabe24490fb0c951c604da33a31d39cc94c0138
Pedro Silva <pedro.silva@mindera.com>
2023-07-03 10:14:25 +0100

    manven checktyle plugin

--- app/src/main/java/com/mindera/randomservice/RandomServiceApplication.java
+++ app/src/main/java/com/mindera/randomservice/RandomServiceApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RandomServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RandomServiceApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(RandomServiceApplication.class, args);
+  }
 
 }

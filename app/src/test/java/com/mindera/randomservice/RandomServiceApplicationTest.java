8e62dc962f50775fde065341272e8818e038363f
Pedro Silva <pedro.silva@mindera.com>
2023-07-11 14:36:57 +0100

    update workflow

---
+++ app/src/test/java/com/mindera/randomservice/RandomServiceApplicationTest.java
@@ -0,0 +1,16 @@
+package com.mindera.randomservice;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(
+    webEnvironment = SpringBootTest.WebEnvironment.MOCK,
+    classes = RandomServiceApplication.class)
+public class RandomServiceApplicationTest {
+
+  @Test
+  public void test() {}
+}
